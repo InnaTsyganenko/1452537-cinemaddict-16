2f11f24d263b229cc1e6269dcf4945e79565a482
Nika <poteryaevainna@gmail.com>
2021-12-18 11:55:42 +0300

    popup view changes

--- src/view/popup-view.js
+++ src/view/popup-view.js
@@ -167,24 +167,6 @@ export default class PopupView extends AbstractView {
   setClosePopupHandler = (callback) => {
     this._callback.click = callback;
     this.element.querySelector('.film-details__close-btn').addEventListener('click', this.#closePopupClickHandler);
-<<<<<<< HEAD
-  }
-
-  setMoviePopupWatchlistClickHandler = (callback) => {
-    this._callback.watchlistClick = callback;
-    this.element.querySelector('.film-details__control-button--watchlist').addEventListener('click', this.#movieWatchlistClickHandler);
-  }
-
-  setMoviePopupWatchedClickHandler = (callback) => {
-    this._callback.watchedClick = callback;
-    this.element.querySelector('.film-details__control-button--watched').addEventListener('click', this.#movieWatchedClickHandler);
-  }
-
-  setMoviePopupFavoriteClickHandler = (callback) => {
-    this._callback.favoriteClick = callback;
-    this.element.querySelector('.film-details__control-button--favorite').addEventListener('click', this.#movieFavoriteClickHandler);
-=======
->>>>>>> 7b419506dba4af66fc1b7bde496d5c0bb8366165
   }
 
   #closePopupClickHandler = (evt) => {
